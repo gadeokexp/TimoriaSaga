3aef8e4fa50eded518fb7db22a072c577911f56e
gadeokexp <gadeokexp@naver.com>
2024-01-12 15:17:28 +0900

    UI작업 인벤토리

---
+++ Assets/Scripts/UI/FieldUI/InventoryPopup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InventoryPopup : GamePopup
+{
+    [SerializeField] public Button InventoryButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UI/FieldUI/SubUIManagerField.cs
+++ Assets/Scripts/UI/FieldUI/SubUIManagerField.cs
@@ -6,6 +6,7 @@ public class SubUIManageField : SubUIManager
 {
     public FieldPannel _fieldPannel;
     public StatusPopup _statusPopup;
+    public InventoryPopup _inventoryPopup;
 
     public override void Active()
     {
@@ -17,6 +18,7 @@ public class SubUIManageField : SubUIManager
 
         _fieldPannel = pannelObject.GetComponent<FieldPannel>();
         _statusPopup = pannelObject.GetComponentInChildren<StatusPopup>();
+        _inventoryPopup = pannelObject.GetComponentInChildren<InventoryPopup>();
 
         if (_fieldPannel == null)
         {
@@ -24,14 +26,33 @@ public class SubUIManageField : SubUIManager
         }
 
         _fieldPannel.SetResolution(_canvasWidth, _canvasHeight);
-        _statusPopup.SetSize(_canvasWidth * 0.2f, _canvasHeight);
 
+        _statusPopup.SetSize(_canvasWidth * 0.2f, _canvasHeight);
         float statusX = _canvasWidth * 0.4f;
         _statusPopup.SetPosition(-statusX, 0);
+        _statusPopup.InventoryButton.onClick.AddListener(ShowInventoryPopup);
+
+        _inventoryPopup.SetSize(_canvasWidth * 0.2f, _canvasHeight * 0.6f);
+        float inventoryX = _canvasWidth * 0.4f;
+        float inventoryY = _canvasHeight * 0.2f;
+        _inventoryPopup.SetPosition(inventoryX, inventoryY);
+        _inventoryPopup.gameObject.SetActive(false);
     }
 
     public override void Deactive()
     {
         base.Deactive();
+    }
+
+    public void ShowInventoryPopup()
+    {
+        if(_inventoryPopup.gameObject.activeSelf)
+        {
+            _inventoryPopup.gameObject.SetActive(false);
+        }
+        else
+        {
+            _inventoryPopup.gameObject.SetActive(true);
+        }
     }
 }
